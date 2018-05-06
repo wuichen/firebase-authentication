bdf01bff89a4b51a89da287be37a115358950fcb
Richard <richard.dimalanta@gmail.com>
2018-05-06 13:40:17 +1000

    Refactor navigation code.

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { withStyles } from "material-ui/styles";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import AppBar from "material-ui/AppBar";
 import Toolbar from "material-ui/Toolbar";
@@ -12,67 +11,33 @@ import * as routes from "../../constants/routes";
 
 import "./Navigation.css";
 
-const styles = {
-  root: {
-    flexGrow: 1
-  },
-  flex: {
-    flex: 1
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
+class Navigation extends Component {
+  render() {
+    return (
+      <div className="Navigation">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit" className="title">
+              Firebase Full Auth
+            </Typography>
+            <AuthUserContext.Consumer>
+              {authUser =>
+                authUser ? (
+                  <SignOutButton />
+                ) : (
+                  <Link to={routes.SIGN_IN} className="signIn">
+                    <Button variant="raised" color="default">
+                      Sign In
+                    </Button>
+                  </Link>
+                )
+              }
+            </AuthUserContext.Consumer>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
   }
-};
+}
 
-const Navigation = props => {
-  return (
-    <AuthUserContext.Consumer>
-      {authUser =>
-        authUser ? (
-          <NavigationAuth {...props} />
-        ) : (
-          <NavigationNonAuth {...props} />
-        )
-      }
-    </AuthUserContext.Consumer>
-  );
-};
-
-const NavigationAuth = props => {
-  const { classes } = props;
-  return (
-    <div className="NavigationNonAuth">
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="title" color="inherit" className={classes.flex}>
-            Firebase Full Auth
-          </Typography>
-          <SignOutButton />
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-};
-
-const NavigationNonAuth = props => {
-  const { classes } = props;
-  return (
-    <div className="NavigationNonAuth">
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="title" color="inherit" className={classes.flex}>
-            Firebase Full Auth
-          </Typography>
-          <Link to={routes.SIGN_IN} className="signIn">
-            <Button variant="raised" color="default">
-              Sign In
-            </Button>
-          </Link>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-};
-
-export default withStyles(styles)(Navigation);
+export default Navigation;
