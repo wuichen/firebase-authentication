82d2c1d7cb22cd339f4f6962d91de65692443270
Richard <richard.dimalanta@gmail.com>
2018-05-26 11:05:40 +1000

    Fixed issue on  re-mounting the component twice.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -39,36 +39,16 @@ class App extends React.Component {
                 <Navigation />
               </header>
               <main>
-                <Route
-                  exact
-                  path={routes.LANDING}
-                  component={() => <LandingPage />}
-                />
-                <Route
-                  exact
-                  path={routes.SIGN_UP}
-                  component={() => <SignUpPage />}
-                />
-                <Route
-                  exact
-                  path={routes.SIGN_IN}
-                  component={() => <SignInPage />}
-                />
+                <Route exact path={routes.LANDING} component={LandingPage} />
+                <Route exact path={routes.SIGN_UP} component={SignUpPage} />
+                <Route exact path={routes.SIGN_IN} component={SignInPage} />
                 <Route
                   exact
                   path={routes.PASSWORD_FORGET}
-                  component={() => <PasswordForgetPage />}
+                  component={PasswordForgetPage}
                 />
-                <Route
-                  exact
-                  path={routes.HOME}
-                  component={() => <HomePage />}
-                />
-                <Route
-                  exact
-                  path={routes.ACCOUNT}
-                  component={() => <AccountPage />}
-                />
+                <Route exact path={routes.HOME} component={HomePage} />
+                <Route exact path={routes.ACCOUNT} component={AccountPage} />
               </main>
             </div>
           </Router>
