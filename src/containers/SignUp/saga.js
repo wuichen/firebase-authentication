57c88dcba83efdc1398422ff56f46b8d0aeed583
Richard <richard.dimalanta@gmail.com>
2018-06-02 10:20:33 +1000

    Added email verification.

--- src/containers/SignUp/saga.js
+++ src/containers/SignUp/saga.js
@@ -8,16 +8,29 @@ import { createUserSuccess, createUserFailed } from "./actions";
 function* handleCreateUser(action) {
   try {
     const { email, username, password } = action;
+
+    //create user in auth firebase
     const authUser = yield call(
       [auth, auth.createUserWithEmailAndPassword],
       email,
       password
     );
+
+    //create user in db
     const ref = db.ref(`users/${authUser.id}`);
     yield call([ref, ref.set], {
       username,
       email
     });
+
+    //send email verification
+    const actionCodeSettings = {
+      url: process.env.REACT_APP_REDIRECT_EMAIL_CONFIRMATION
+    };
+    yield call(
+      [auth.currentUser, auth.currentUser.sendEmailVerification],
+      actionCodeSettings
+    );
     yield put(createUserSuccess());
   } catch (e) {
     console.log("error", e);
